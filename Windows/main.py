16eceb1edc745132bc4f2cf9c0cb6f7022ee378e
KCIRREM <102417215+KCIRREM@users.noreply.github.com>
2022-04-06 20:07:42 +0100

    updated abums work
    album tests still in beta also re usability still in beta as you cannot add more songs to a playlist thet you've downloaded through downloading the same playlist with spotify
    note: albums still in beta not tested with disks will finish if i have the time

--- Windows/main.py
+++ Windows/main.py
@@ -32,47 +32,56 @@ output = []
 thumbnail_list = []
 playlist_file = []
 
-url = input('Enter your spotify playlist/album link')
+url = input('Enter the spotify playlist')
 res = requests.get(url)
 html_page = res.content
 soup = BeautifulSoup(html_page, 'lxml')
-soup1 = BeautifulSoup(html_page)
 text = soup.find_all(text=True)
 elements_of_page = set([t.parent.name for t in text])
 #print(elements_of_page)
 blacklist = ['title','[document]','style','h1','script']
             #{'script','h1','[document]','title','style'}
-i = 0
-for t in range(0,len(text)):
-    if text[t].parent.name not in blacklist:
-        #print(text[t])
-        if text[t].isdigit() == True:
-            output.append([text[t]])
-            i = t + 1
-            #print(i)
+
+for idx, val in enumerate(text):
+    if val.parent.name not in blacklist:
+        if val.isdigit() == True:
+            output.append([val])
+            i = idx + 1
             try:
                 while text[i].isdigit() == False:
                     output[len(output)-1].append(text[i])
-                    i = i + 1
-            except Exception as Argument:
+                    i = i +1
+            except Exception as IndexError:
                 break
-num = output[len(output)-1].index('You might also like')
-what_to_delete = output[len(output)-1]
-del what_to_delete[num:]
 url_output = str((urllib.request.urlopen(url)).read().decode())
-url_urls = (url_output.split(((re.search('https://i.scdn.co/image/[a-zA-Z0-9]+',url_output)).group(0))))
-url_urls = url_urls.pop((len(url_urls)-1))
-spotify_urls = re.findall('https://open.spotify.com/track/[a-zA-Z0-9]+',url_urls)
+if ((''.join((url.split('https://open.spotify.com/')).pop(1))).split('/',1)).pop(0) == 'playlist':
+    #print(output)
+    num = output[len(output)-1].index('You might also like')
+    what_to_delete = output[len(output) - 1]
+    del what_to_delete[num:]
+    url_urls = (url_output.split(((re.search('https://i.scdn.co/image/[a-zA-Z0-9]+', url_output)).group(0))))
+    url_urls = url_urls.pop((len(url_urls) - 1))
+else:
+    num = output[(len(output))-1].index(output[len(output) - 2][(len(output[len(output) - 2]))-1])
+    what_to_delete = output[len(output) - 1]
+    del what_to_delete[num:]
+    del output[0]
+    url_urls = url_output
 
+print(output)
+spotify_urls = re.findall('https://open.spotify.com/track/[a-zA-Z0-9]+',url_urls)
+print(len(spotify_urls))
 print(spotify_urls)
 path = (filedialog.askdirectory()+'/')
 list_of_pre_existing_files = os.listdir(path)
 print(list_of_pre_existing_files)
 #print(str((urllib.request.urlopen(spotify_urls[0])).read().decode()))
+
 for i in range(0,len(spotify_urls)):
     thumbnail_track = re.search('https://i.scdn.co/image/[a-zA-Z0-9]+',(str((urllib.request.urlopen(spotify_urls[i])).read().decode()))).group(0)
     thumbnail_list.append(thumbnail_track)
     print('Downloading track: ', i)
+    print(('"'+(((' '.join([output[i][item] for item in range(0,len(output[i])) if output[i][item].isdigit()==False])).replace(" ", "+"))+'"')))
     youtube_query = urllib.request.urlopen(
         'https://www.youtube.com/results?search_query=' + ('"'+(((' '.join([output[i][item] for item in range(0,len(output[i])) if output[i][item].isdigit()==False])).replace(" ", "+"))+'"')))
     video_id = 'https://www.youtube.com/' + (re.search(r"watch\?v=(\S{11})", youtube_query.read().decode())).group(
@@ -80,6 +89,7 @@ for i in range(0,len(spotify_urls)):
     with yt_dlp.YoutubeDL((options(path))) as ydl:
         ydl.download(video_id)
 
+    os.rename((path + 'download_archive'),(path + 'download_archive.txt'))
     list_of_new_files = os.listdir(path)
     file_name = ''.join(set(list_of_new_files)-set(list_of_pre_existing_files))
     print(file_name)
@@ -90,10 +100,11 @@ for i in range(0,len(spotify_urls)):
         os.rename((path+file_name),(path + (output[i][1])+ '.mp3'))
     except Exception as FileExistsError:
         pass
-    with open(path + 'download_archive', 'r')  as file:
+
+    with open(path + 'download_archive.txt', 'r')  as file:
         downloaded = file.readlines()
     downloaded[i] += (output[i][1] + '\n')
-    with open(path + 'download_archive','w') as file:
+    with open(path + 'download_archive.txt','w') as file:
         file.writelines(downloaded)
 
     print((path + (output[i][1])+ '.mp3'))
@@ -103,7 +114,6 @@ for i in range(0,len(spotify_urls)):
     print(list_of_pre_existing_files)
     print(list_of_new_files)
 
-print(text[0])
 path_to_m3u = (filedialog.askdirectory()) + '/'
 name = ''.join([item for item in text if item.parent.name == 'title'])
 if os.path.exists(path_to_m3u  + name + '.m3u') == True:
@@ -111,3 +121,9 @@ if os.path.exists(path_to_m3u  + name + '.m3u') == True:
 else:
     with open((path_to_m3u  + name + '.m3u'), 'w',) as file:
         file.writelines(playlist_file)
+
+print('songs downloaded:')
+for idx, val in enumerate(output):
+    for idx2, val2 in enumerate(output[idx]):
+        if idx2 == 1:
+            print(val2)
