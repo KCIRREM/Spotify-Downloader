3ac22c1af5a008e5403aa4aac0f325658a49a1d8
KCIRREM <102417215+KCIRREM@users.noreply.github.com>
2022-04-07 20:05:46 +0100

    Delete rename-bash.sh

--- Android/rename-bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-input="/storage/emulated/0/Download/YT Music/temp.txt"
-while IFS= read -r line || [[ -n "$line" ]]; do
-    str="a"
-	newname="$line$str"
-	mv "$line" "$newname"
-	echo "Text read from file: $line"
-done < "$input"
